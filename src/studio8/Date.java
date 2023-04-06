c53acac122341a8b3181057c38df713da27e5910
OrionStrayer <o.strayer@wustl.edu>
2023-04-06 13:42:37 -0500

    Done studio 8!

--- src/studio8/Date.java
+++ src/studio8/Date.java
@@ -1,10 +1,81 @@
 package studio8;
 
+import java.util.LinkedList;
+import java.util.Objects;
+
 public class Date {
 
+	private int month;
+	private int day;
+	private int year;
+	private boolean holiday;
+	
+	
+    /**
+	 * @param month
+	 * @param day
+	 * @param year
+	 * @param holiday
+	 */
+	public Date(int month, int day, int year, boolean holiday) {
+		this.month = month;
+		this.day = day;
+		this.year = year;
+		this.holiday = holiday;
+	}
 
-    public static void main(String[] args) {
 
+
+	@Override
+	public String toString() {
+		return "[" + month + "/" + day + "/" + year + "]";
+	}
+
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(day, month, year);
+	}
+
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Date other = (Date) obj;
+		return day == other.day && month == other.month && year == other.year;
+	}
+
+
+
+	public static void main(String[] args) {
+
+		Date date1 = new Date(1, 31, 2000, false);
+		Date date2 = new Date(8, 17, 2001, false);
+		Date date3 = new Date(1, 31, 2000, false);
+		Date date4 = new Date(9, 21, 2001, false);
+		Date date5 = new Date(6, 15, 1953, true);
+		
+		System.out.println(date1);
+		System.out.println(date2);
+		
+		System.out.println(date1.equals(date2));
+		System.out.println(date1.equals(date3));
+		
+		LinkedList<Date> list = new LinkedList<Date>();
+		list.add(date1);
+		list.add(date2);
+		list.add(date3);
+		list.add(date4);
+		list.add(date5);
+    	
+		System.out.println(list);
     }
 
 }
--- src/studio8/Time.java
+++ src/studio8/Time.java
@@ -1,11 +1,94 @@
 package studio8;
 
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Objects;
 
 public class Time {
 
+	private int hour;
+	private int minute;
+	private boolean twelveHrFormat;
+	
+/**
+ * @param hour
+ * @param minute
+ * @param twelveHrFormat
+ */
+	public Time(int hour, int minute, boolean twelveHrFormat) {
+		this.hour = hour;
+		this.minute = minute;
+		this.twelveHrFormat = twelveHrFormat;
+	}
+
+	
+	
+	@Override
+	public String toString() {
+		return hour + ":" + minute + " - " + twelveHrFormat;
+	}
+
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(hour, minute);
+	}
+
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Time other = (Time) obj;
+		return hour == other.hour && minute == other.minute;
+	}
+
+
+
 	public static void main(String[] args) {
  
+		Time time1 = new Time(4, 36, true);
+		Time time2 = new Time (3, 14, true);
+		Time time3 = new Time(4, 36, true);
+		Time time4 = new Time(5, 01, true);
+		Time time5 = new Time(9, 00, false);
+		
+		System.out.println(time1);
+		System.out.println(time2);
+		
+		System.out.println(time1.equals(time2));
+		System.out.println(time1.equals(time3));
+		
+		LinkedList<Time> list = new LinkedList<Time>();
+		
+		list.add(time1);
+		list.add(time2);
+		list.add(time3);
+		list.add(time4);
+		list.add(time5);
+		
+		System.out.println(list);
+		
+		HashSet<Time> set = new HashSet<Time>();
+		
+		set.add(time1);
+		set.add(time2);
+		set.add(time3);
+		set.add(time4);
+		set.add(time5);
+		
+		System.out.println(set);
+		
+		
+
     	
     }
+
 
 }
